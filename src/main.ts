e96420585730cbc418d09759836a30b843df1bc3
Nurken <nurken.serdakov.2000@mail.ru>
2026-02-09 15:05:58 +0500

    database integration

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function start(){
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('v1');
+
     await app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
 
 }
